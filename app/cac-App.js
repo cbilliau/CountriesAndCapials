6d2bbdd6e064da8b8b5579e59793e698ed493513
cbilliau <christopher@billiau.com>
2016-08-23 16:51:29 -0400

    Update 2016-08-23T20:51:27.711Z

--- app/cac-App.js
+++
@@ -1,7 +0,0 @@
-angular.module('cacApp', ['cacAppViews', 'ngRoute', 'ngAnimate'])
-	.config(function($locationProvider, $routeProvider)	{
-		$locationProvider.hashPrefix('!');
-		$routeProvider.otherwise({
-			redirectTo : '/'
-		});
-	});
--- app/cac-app-views.js
+++
@@ -1,2 +0,0 @@
-var viewsModule = angular.module('cacAppViews',
-	['ngRoute', 'cacLibrary']);
