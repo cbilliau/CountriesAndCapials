25473911b059dab133c7dab72123f3d2404b968b
cbilliau <christopher@billiau.com>
2016-08-26 11:13:19 -0400

    Test homepage html, success.

--- tests/unit/spec.js
+++ tests/unit/spec.js
@@ -1,4 +1,38 @@
-describe('Home page test', function() {
+// Route tests
+xdescribe('Home page route test', function() {
+
+  beforeEach(function() {
+    module('cacApp');
+  });
+
+  beforeEach(inject(function(_$httpBackend_, _$route_, _$location_, $rootScope)  {
+    $httpBackend = _$httpBackend_;
+    $route = _$route_;
+    $location = _$location_;
+    $scope = $rootScope.$new();
+    string = '<p>Error - Page Not Found</p>';
+  }));
+
+  it('should load the home.html template', function(){
+    $httpBackend.whenGET('./home/home.html').respond('...');
+    $scope.$apply(function() {
+      $location.path('/home');
+    });
+    expect($route.current.templateUrl).toBe('./home/home.html');
+    expect($route.current.controller).toBe('HomeCtrl');
+  });
+
+  it('should load the error template when `/error` reached', function(){
+    $httpBackend.whenGET(string).respond('...');
+    $scope.$apply(function() {
+      $location.path('/error');
+    });
+    expect($route.current.template).toBe(string);
+  });
+
+});
+
+describe('Countries page route test', function() {
 
   beforeEach(function() {
     module('cacApp');
@@ -11,12 +45,13 @@ describe('Home page test', function() {
     $scope = $rootScope.$new();
   }));
 
-  it('should load the home.html template', function(){
-    $httpBackend.whenGET('./home/home.html').respond('...');
+  it('should load countries.html template', function(){
+    $httpBackend.whenGET('./countries/countries.html').respon('...');
     $scope.$apply(function() {
-      $location.path('/home');
+      $location.path('/countries');
     });
-    expect($route.current.templateUrl).toBe('./home/home.html');
-    expect($route.current.controller).toBe('HomeCtrl');
+    expect($route.current.templateUrl).toBe('./countries/countries.html');
+    expect($route.current.controller).toBe('CountriesCtrl');
   });
+
 });
